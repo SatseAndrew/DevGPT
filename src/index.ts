2c6ecde31be489d28164c6e1d38a77e198b55199
Andrew Nielsen <andrew.nielsen@sats.no>
2023-05-15 23:22:01 +0200

    Initialize CLI with Commander in index.ts
    The 'index.ts' file has been updated to import and set up the Commander package as the main command line interface for the program.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('👋 Hello, world!')
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+    .name('dev-gpt')
+    .description('An application that integrates GPT with your development workflow, assisting with Git operations, code review and debugging.')
+    .version('0.0.1');
+
+program.parse(process.argv);
