f74209ce39d6fb804577fc5984bc0f051a67857d
Andrew Nielsen <andrew.nielsen@sats.no>
2023-05-16 22:18:00 +0200

    Add generate git-message command
    This command reads the staged changes using the 'getStagedChanges' function from the 'git' module, and prompts Chat-GPT to write a Git commit message based on those changes.
    
    The 'generateCommitMessage' function also reads a system message from the 'systemMessageUtils' module, and displays it to the user before prompting for the commit message.
    
    Additionally, this commit imports and configures the dotenv module to read environment variables from a .env file, and adds the 'generateCommitMessage' command to the application's command line interface (CLI) using the 'commander' module.
    
    This feature will help developers to write more descriptive and informative Git commit messages, and to follow best practices for version control.

---
+++ src/commands/generateCommitMessage.ts
@@ -0,0 +1,35 @@
+import { getStagedChanges  } from "../git";
+import fs from 'fs';
+import { SystemMessage, readSystemMessage } from "../utils/readSystemMessage";
+import { ChatMessage } from "../interfaces";
+import { startChatLoop } from "../gpt";
+
+export async function generateCommitMessage() {
+    const stagedChanges = await getStagedChanges();
+
+    if (!stagedChanges) {
+        console.log('No changes staged for commit.');
+        return;
+    }
+
+    let systemMessage: string;
+    try {
+        systemMessage = await readSystemMessage(SystemMessage.GitAssistant);
+    } catch (error) {
+        console.error('Failed to read system message.', error);
+        return;
+    }
+    
+    let initialMessages: ChatMessage[] = [
+        {
+            "role": "system",
+            "content": systemMessage
+        },
+        {
+            "role": "user",
+            "content": `Please write a git commit message based on the following changes:\n${stagedChanges}`
+        }
+    ];
+
+    await startChatLoop(initialMessages);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { Command } from 'commander';
+import { generateCommitMessage } from './commands/generateCommitMessage';
 
 const program = new Command();
 
@@ -6,5 +9,13 @@ program
     .name('dev-gpt')
     .description('An application that integrates GPT with your development workflow, assisting with Git operations, code review and debugging.')
     .version('0.0.1');
+
+program
+    .command('generate <type>')
+    .action(async (type: string) => {
+        if (type === 'git-message') {
+            await generateCommitMessage();
+        }
+    });
 
 program.parse(process.argv);
