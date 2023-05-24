f3676a78637eff4d92b935ff52b3adb6e0643cc0
Andrew Nielsen <andrew.nielsen@sats.no>
2023-05-24 19:59:56 +0200

    Add reviewCodeFile command
    This commit adds a new command called reviewCodeFile to the application. The command loads the content of a file specified by its file path and sends it for review to a chatbot using the GPT-3 API.

---
+++ src/commands/reviewCodeFile.ts
@@ -0,0 +1,48 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { SystemMessage, readSystemMessage } from '../utils/readSystemMessage';
+import { ChatMessage } from '../interfaces';
+import { startChatLoop } from '../gpt';
+
+export async function reviewCodeFile(filepath: string) {
+    const cwd = process.cwd();
+    const fullpath = path.resolve(cwd, filepath);
+
+    try {
+        fs.access(fullpath);
+    } catch {
+        console.error(`File does not exist: ${fullpath}`);
+        return;
+    }
+
+    let code: string;
+
+    try {
+        code = await fs.readFile(fullpath, 'utf-8');
+    } catch(error) {
+        console.error(`Readings contents from file failed with error: ${error}`)
+        return;
+    }
+
+    let systemMessage: string;
+
+    try {
+        systemMessage = await readSystemMessage(SystemMessage.CodeReviewer);
+    } catch(error) {
+        console.error('Failed to read system message.', error);
+        return;
+    }
+
+    let initialMessages: ChatMessage[] = [
+        {
+            "role": "system",
+            "content": systemMessage
+        },
+        {
+            "role": "user",
+            "content": `Please review this code and give me feedback:\n${code}`
+        }
+    ];
+
+    await startChatLoop(initialMessages);
+}
