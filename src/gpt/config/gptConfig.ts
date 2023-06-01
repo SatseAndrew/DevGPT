bd751d84ad5edb563aad4f4d12941379ce897a90
Andrew Nielsen <andrew.nielsen@sats.no>
2023-06-01 13:48:42 +0200

    Add constant MAX_TOKEN_SIZE to `gptConfig.ts`

--- src/gpt/config/gptConfig.ts
+++ src/gpt/config/gptConfig.ts
@@ -6,3 +6,4 @@ export const OPENAI_API_KEY = process.env['OPENAI_API_KEY'];
 export const MODEL = 'gpt-3.5-turbo';
 export const OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
 export const TEMPERATURE = 0.7;
+export const MAX_TOKEN_SIZE = 15000;
